eb5b3729efa7355a0447cca41b5d56d5de8b86ac
bg7jws <szdataio@163.com>
2020-08-25 21:50:30 +0800

    move to sub-dir

---
+++ file1/1.asm
@@ -0,0 +1,40 @@
+;ml /c /coff filename
+;link /subsystem:console filename
+.386
+.model flat, stdcall
+option casemap:none
+include windows.inc
+include kernel32.inc
+include user32.inc
+includelib kernel32.lib
+includelib user32.lib
+.data
+a		db 10 dup(0)
+buffer	db 20 dup(0)
+Caption	db 'Êä³ö', 0
+szFmt	db 'The Result is %d', 0
+i		db 0
+sum		db 0
+.code
+start:
+	mov edi, 0
+	.while (i < 10)
+		mov al, i
+		mov a[edi], al
+		inc i
+		inc edi
+	.endw
+	mov i, 0
+	mov edi, 0
+	.while (i < 10)
+		mov al, a[edi]
+		add sum, al
+		inc i
+		inc edi
+	.endw
+	xor eax, eax
+	mov al, sum
+	invoke wsprintf, addr buffer, addr szFmt, eax
+	invoke MessageBox, NULL, offset buffer, offset Caption, MB_OK
+	invoke ExitProcess, 0
+end start
--- makeit.bat
+++
@@ -1,4 +0,0 @@
-ml /c /coff  %1.asm
-link /subsystem:windows %1.obj
-del %1.obj
-dir %1.*
