912e4ed53ca1ddff4683f311333029bf7bdeb7ee
bg7jws <38001684+bg7jws@users.noreply.github.com>
2021-06-28 16:32:53 +0800

    change directory name
    change directory name

--- file6/var.bat
+++
@@ -1,5 +0,0 @@
-set masm32dir=c:\masm32
-set include=%masm32dir%\include;
-set lib=%masm32dir%\lib;%lib%
-set path=%masm32dir%\bin;%masm32dir%\include;%masm32dir%\lib;%masm32dir%;%path%
-set masm32dir=
